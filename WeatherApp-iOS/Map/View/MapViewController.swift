bdc58a395b4687fb7c6b1c76cc381879dc9af03d
mohamedtahoon <mohmaed.tahoon15@gmail.com>
2019-08-05 21:37:05 +0200

    Adding Search for places in Map

--- WeatherApp-iOS/Map/View/MapViewController.swift
+++ WeatherApp-iOS/Map/View/MapViewController.swift
@@ -11,7 +11,7 @@ import MapKit
 import CoreLocation
 
 
-class MapViewController: UIViewController  {
+class MapViewController: UIViewController, UISearchBarDelegate  {
     let network: NetworkManager = NetworkManager.sharedInstance
     let alert = Alert()
     
@@ -19,6 +19,79 @@ class MapViewController: UIViewController  {
     
     @IBOutlet weak var mapView: MKMapView!
     let alertView : UIView = UIView()
+    
+    //Search in Map
+    @IBAction func searchBtn(_ sender: Any) {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.delegate = self
+        present(searchController, animated: true, completion: nil)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
+    {
+        //Ignoring user
+        UIApplication.shared.beginIgnoringInteractionEvents()
+        
+        //Activity Indicator
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.style = UIActivityIndicatorView.Style.gray
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating()
+        
+        self.view.addSubview(activityIndicator)
+        
+        //Hide search bar
+        searchBar.resignFirstResponder()
+        dismiss(animated: true, completion: nil)
+        
+        //Create the search request
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = searchBar.text
+        
+        let activeSearch = MKLocalSearch(request: searchRequest)
+        
+        activeSearch.start { (response, error) in
+            
+            activityIndicator.stopAnimating()
+            UIApplication.shared.endIgnoringInteractionEvents()
+            
+            if response == nil
+            {
+                self.alert.showAlert(title: "Error", message: "Sorry, the City Name you entered is not correct", actions:[(title:"Try Again",action: {
+                    self.dismiss(animated: true, completion: nil)
+                })])
+                print("ERROR")
+            }
+            else
+            {
+                //Remove annotations
+                let annotations = self.mapView.annotations
+                self.mapView.removeAnnotations(annotations)
+                
+                //Getting data
+                let latitude = response?.boundingRegion.center.latitude
+                let longitude = response?.boundingRegion.center.longitude
+                
+                //Create annotation
+                
+                //let annotation = MKPointAnnotation()
+                //annotation.title = searchBar.text
+                //annotation.coordinate = CLLocationCoordinate2DMake(latitude!, longitude!)
+                //self.mapView.addAnnotation(annotation)
+                
+                //Zooming in on annotation
+                let coordinate:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude!, longitude!)
+                let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+                let region = MKCoordinateRegion(center: coordinate, span: span)
+                self.mapView.setRegion(region, animated: true)
+            }
+            
+        }
+    }
+    
+    
+    
     
     //MARK: - Life Cycle
     override func viewDidLoad() {
